335cee40cb0136fe8ef4f9e6a82519e9a81fdfda
vahra <vahra@DESKTOP-SO0L1VA>
2022-07-13 18:23:27 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public static int Sum(int []b,int c)
+    {
+       for(int i = 0; i < b.Length; i++)
+        {
+            c=b[i]+ c;
+        }
+        return c;
+    }
+    static void Main(string[] args)
+    {
+        
+        int t = Convert.ToInt32(Console.ReadLine());
+        int []arr = new int[t];
+        for (int i = 0; i < t; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+      
+        Console.WriteLine(Sum(arr, 0));
+        
+    }
+}
